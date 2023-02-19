a2c9d9d785e9bd20aa63ed9e67faf3ff99b1bf8e
dragonappear <dragonappear1@gmail.com>
2023-02-19 11:12:32 +0900

    .

--- mst/BOJ_물대기.py
+++ mst/BOJ_물대기.py
@@ -10,8 +10,7 @@ def find(a):
 
 
 def union(a, b):
-    x = find(a)
-    y = find(b)
+    x, y = find(a), find(b)
     if x < y:
         parent[y] = x
     else:
@@ -21,13 +20,12 @@ def union(a, b):
 N = int(input())
 edges = set()
 for i in range(N):
-    cost = int(input())
-    edges.add((cost, i, N))
+    edges.add((int(input()), i, N))
 
 P = [list(map(int, input().strip().split())) for _ in range(N)]
 
-for i in range(N):
-    for j in range(N):
+for i in range(N-1):
+    for j in range(i+1, N):
         if i == j:
             continue
         edges.add((P[i][j], i, j))
@@ -35,14 +33,14 @@ for i in range(N):
 edges = sorted(list(edges))
 parent = [i for i in range(N+1)]
 
-ans = v = 0
+ans = e = 0
 for edge in edges:
-    cost, a, b = edge
+    c, a, b = edge
     if find(a) == find(b):
         continue
     union(a, b)
-    ans += cost
-    v += 1
-    if v == N:
+    ans += c
+    e += 1
+    if e == N:
         break
 print(ans)
