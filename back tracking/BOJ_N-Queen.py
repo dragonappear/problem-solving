83b4a129200307a888b45615c0bed6b7a75125ff
dragonappear <dragonappear1@gmail.com>
2023-03-06 12:48:26 +0900

    .

--- back tracking/BOJ_N-Queen.py
+++ back tracking/BOJ_N-Queen.py
@@ -1,24 +1,31 @@
 # https://www.acmicpc.net/problem/9663
-from sys import stdin,stdout
-input,write = stdin.readline,stdout.write
+from sys import stdin
+input = stdin.readline
 
 
-def queen(row):
-    global cnt
-    
-    if row==N:
-        cnt+=1
-        return 
-    
-    for col in range(N):
-        # 행,/,\ 체크
-        if vertical[col] or  diag1[row+col] or diag2[row-col+N-1] : continue
-        vertical[col]=diag1[row+col]=diag2[row-col+N-1]=True
-        queen(row+1)
-        vertical[col]=diag1[row+col]=diag2[row-col+N-1]=False
-        
-N=int(input())
-cnt=0
-vertical,diag1,diag2=[False]*(N),[False]*(2*N),[False]*(2*N)
-queen(0)
-print(cnt)
+def dfs(row):
+    global ans
+
+    if row == N:
+        ans += 1
+        return
+
+    for i in range(N):
+        if col[i] or diag1[row+i] or diag2[row-i+N-1]:
+            continue
+
+        col[i] = diag1[row+i] = diag2[row-i+N-1] = True
+        dfs(row+1)
+        col[i] = diag1[row+i] = diag2[row-i+N-1] = False
+
+
+N = int(input())
+
+ans = 0
+col = [False]*(N+1)
+diag1 = [False]*(2*N)
+diag2 = [False]*(2*N)
+
+dfs(0)
+
+print(ans)
--- bfs/BOJ_말이_되고픈_원숭이.py
+++ bfs/BOJ_말이_되고픈_원숭이.py
@@ -1,37 +1,47 @@
 # https://www.acmicpc.net/problem/1600
-from sys import stdin,stdout
+from sys import stdin
 from collections import deque
-input,write=stdin.readline,stdout.write
+input = stdin.readline
+
 
 def bfs():
-    def OOB(nr,nc): return True if not(0<=nr<N) or not(0<=nc<M) else False
-    dist=[[[-1]*M for _ in range(N)] for _ in range(K+1)] # dist[k][i][j]: (i,j)까지 k번 사용해서 도달한 최단거리
-    dist[0][0][0]=0
-    q=deque([(0,0,0)])
-        
+
+    if board[0][0] == 1:
+        return -1
+
+    dist = [[[-1] * W for _ in range(H)] for _ in range(K+1)]
+    q = deque([(0, 0, 0)])
+    dist[0][0][0] = 0
+
     while q:
-        r,c,k=q.popleft()
-        
-        if r==N-1 and c==M-1:
+        r, c, k = q.popleft()
+
+        if (r, c) == (H-1, W-1):
             return dist[k][r][c]
-        
-        if k<K:            
-            for dr,dc in dh:
-                nr,nc=r+dr,c+dc
-                if OOB(nr,nc) or graph[nr][nc]==1 or dist[k+1][nr][nc]>=0: continue
-                dist[k+1][nr][nc]=dist[k][r][c]+1
-                q.append((nr,nc,k+1))
-        
-        for dr,dc in dr_dc:
-            nr,nc=r+dr,c+dc
-            if OOB(nr,nc) or graph[nr][nc]==1 or dist[k][nr][nc]>=0: continue
-            dist[k][nr][nc]=dist[k][r][c]+1
-            q.append((nr,nc,k))
+
+        if k < K:
+            for dr, dc in [(-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, -2), (2, -1), (2, 1), (1, 2)]:
+                nr, nc = r+dr, c+dc
+
+                if not(0 <= nr < H) or not(0 <= nc < W) or board[nr][nc] or dist[k+1][nr][nc] > -1:
+                    continue
+
+                dist[k+1][nr][nc] = dist[k][r][c]+1
+                q.append((nr, nc, k+1))
+
+        for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nr, nc = r+dr, c+dc
+
+            if not(0 <= nr < H) or not(0 <= nc < W) or board[nr][nc] or dist[k][nr][nc] > -1:
+                continue
+
+            dist[k][nr][nc] = dist[k][r][c]+1
+            q.append((nr, nc, k))
+
     return -1
 
-K=int(input())
-M,N=map(int,input().split())
-graph=[list(map(int,input().split()))  for _ in range(N)]
-dr_dc=[(0,1),(0,-1),(1,0),(-1,0)]
-dh=[(-1,-2),(-2,-1),(-2,1),(-1,2),(1,-2),(2,-1),(2,1),(1,2)]
+
+K = int(input())
+W, H = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(H)]
 print(bfs())
